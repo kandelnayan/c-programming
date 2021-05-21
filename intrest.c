f4d3e43df3a306e0dfc352d92bf3f06ec107f707
kandelnayan <84581895+kandelnayan@users.noreply.github.com>
2021-05-21 20:51:12 +0545

    interest.c file add

---
+++ intrest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*===INVESTMENT PROBLEM=*/
+#define PERIOD 10
+#define PRINCIPAL 5000
+
+
+main(){
+	int year;
+	float amount, value, inrate;
+	
+	amount = PRINCIPAL;
+	inrate = 0.11;
+	year = 0;
+	
+	while(year <=PERIOD){
+		printf("%2d  %8.2f\n" ,year ,amount);
+		value = amount + inrate * amount;
+		year = year + 1;
+		amount = value;
+	}
+	
+	
+}
