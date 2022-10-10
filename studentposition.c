18936ff58bcd18ffcb145212457f766e3e75d4ef
kandelnayan <84581895+kandelnayan@users.noreply.github.com>
2022-10-10 13:09:07 +0545

    Add files via upload

---
+++ studentposition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int percent;
+	printf("enter your percent : \n");
+	scanf("%d",& percent);
+	
+	if(percent>= 50){
+		  printf("Congratulation you have pass the exam");
+	  } else {
+		  printf("sorry you have fail the exam");
+	  } return 0;}
+	  
+	  
+ 
