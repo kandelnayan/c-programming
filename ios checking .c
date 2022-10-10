204e76b0a9613736c1d3d2b75460f7acf383e0d9
kandelnayan <84581895+kandelnayan@users.noreply.github.com>
2022-10-10 14:15:52 +0545

    Create ios checking .c

---
+++ ios checking .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() {
+	 int ios;
+	 printf("enter your ios : \n");
+	 scanf("%d", &ios);
+	 
+	 if(ios>= 10) {
+		 printf("you iphone is latest");
+	 }else { printf("you iphone is old");
+	  } return 0;
+  }
